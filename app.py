a9753e7b0298e4133be79cfd7ef987aec9c88045
Gustavo Michelsen <gtv.michelsen@gmail.com>
2023-02-04 12:38:49 -0300

    adjusted the data structure

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import numpy as np
 import streamlit as st
 
 def load_data():
-    return pd.read_csv('data/processed/bikes_completed.csv')
+    return pd.read_csv('data/bikes_completed.csv')
 
 def main()
     df = load_data()
